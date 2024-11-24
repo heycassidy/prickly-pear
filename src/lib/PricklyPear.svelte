4d20eed1d8cd9db6608adce2ffc3d039dab60ad0
Cassidy Smith <cassidysmith319@gmail.com>
2024-11-24 12:33:36 -0600

    upgrade to svelte 5

--- src/lib/PricklyPear.svelte
+++ src/lib/PricklyPear.svelte
@@ -1,12 +1,11 @@
 <script>
   import generateCactus from './generateCactus.js';
 
-  let className;
   // creates a `class` property, even
   // though it is a reserved word
-  export { className as class };
-  export let color;
+  
+  let { class: className, color } = $props();
 
 </script>
 
-<canvas use:generateCactus={{ color }} class={`${className || ''}`} />
+<canvas use:generateCactus={{ color }} class={`${className || ''}`}></canvas>
